42f05aa1bcec23aa93771e8ea294c68656c024c8
Robert Si <robertsi0214@gmail.com>
2017-12-01 00:31:06 -0800

    complete dayOfTheYear

--- 7kyu/dayOfTheYear.js
+++ 7kyu/dayOfTheYear.js
@@ -9,6 +9,22 @@
 
 
 
+//input is an array with 3 integers date month year
+
 function toDayOfYear(arr) {
   // return a number
+  var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  var count = 0;
+  for (var i = 0; i < arr[1] - 1; i++) {
+    count += daysInMonth[i]
+  }
+  count = count + arr[0];
+  if (arr[1] > 2) {
+    if (arr[2] % 4 === 0) {
+      arr[2] % 100 ? count++ : count
+    }
+  }
+  return count;
 }
+
+toDayOfYear([1,5,2600])
