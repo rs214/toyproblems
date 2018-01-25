ed172b4e3279109d60545a593fdb8bc5028a7db5
rs214 <robertsi0214@gmail.com>
2018-01-24 23:44:09 -0800

    complete naive solutionfor prime tester

--- hrr23 toy problems/primeTester/primeTester.js
+++ hrr23 toy problems/primeTester/primeTester.js
@@ -5,7 +5,20 @@
  */
 
 var primeTester = function(n) {
-
+  if (n === 1) {
+    return false
+  }
+  else if (n === 2) {
+    return true
+  }
+  else {
+    for (var i = 2; i < n; i++) {
+      if (n % x === 0) {
+        return false
+      }
+    }
+    return true;
+  }
 };
 
 
